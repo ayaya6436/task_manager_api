f2022de4e130dc3b00ff831d9cfb27f0c59e0806
ayaya <ayaya6436@gmail.com>
2025-11-15 22:15:42 +0000

    configure project and define constant

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Task Manager API!")
+	router := gin.Default()
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
 }
